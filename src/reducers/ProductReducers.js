bcf51c448b1eaff587cb9d8b8e4405f7fb8ef7ec
dong uk <gusrn1255@naver.com>
2021-07-10 11:11:06 +0900

    product action constans

---
+++ src/reducers/ProductReducers.js
@@ -0,0 +1,29 @@
+import {
+    PRODUCT_LIST_FAIL,
+    PRODUCT_LIST_SUCESS,
+    PRODUCT_LIST_REQUEST
+} from "../contants/ProductsConstants"
+
+export const productListReducers = (state = {products: []}, action ) => {
+    switch (action.type) {
+        case PRODUCT_LIST_REQUEST :
+            return {
+                loading : true,
+                products : []
+            }
+        case PRODUCT_LIST_SUCESS :
+            return {
+                loading: false,
+                products: action.payload
+            }
+
+        case PRODUCT_LIST_FAIL :
+            return {
+                loading: false,
+                products: action.payload
+            }
+
+        default : 
+            return state
+    }
+}
